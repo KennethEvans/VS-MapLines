7d384481a1c43247607311738453d0881803b781
Kenneth Evans <ken@kenevans.net>
2020-11-22 23:20:56 -0500

    Used a different Program.cs that runs MainForm in a top-level STA thread.
    Version is 2.1.0.0.

--- Map Lines/Program.cs
+++ Map Lines/Program.cs
@@ -1,17 +1,12 @@
-using System;
+using System.Threading;
 using System.Windows.Forms;
+using MapLines;
 
-namespace MapLines {
-    static class Program {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main() {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
-    }
-}
+var thread = new Thread(() => {
+    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+    Application.EnableVisualStyles();
+    Application.SetCompatibleTextRenderingDefault(false);
+    Application.Run(new MainForm());
+});
+thread.SetApartmentState(ApartmentState.STA);
+thread.Start();
